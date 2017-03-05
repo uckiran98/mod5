217783e8bdb4891471a9535832413fca097a79bc
Lawrence McDaniel <lpm0073@gmail.com>
2017-03-05 09:51:22 -0600

    completed project

--- js/script.js
+++ js/script.js
@@ -83,7 +83,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  //[...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  // ----------------------------------------------
+  buildAndShowHomeHTML,
+  // ----------------------------------------------
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -103,6 +106,9 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
 
+      // ----------------------------------------------
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      // ----------------------------------------------
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -117,11 +123,20 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
 
+      // ----------------------------------------------
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+      // ----------------------------------------------
+
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+      // ----------------------------------------------
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+      // ----------------------------------------------
+
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
